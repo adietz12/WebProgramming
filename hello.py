50a840faee104fcc320c76d47f9da2cf16c61cfa
adietz12 <150217383+adietz12@users.noreply.github.com>
2024-02-15 22:57:33 +0000

    New stuff

--- hello.py
+++ hello.py
@@ -1,7 +1,33 @@
-from flask import Flask
+from flask import Flask, jsonify, send_from_directory
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def get_index():
+    me ="Anthony"
+    return f"<p>Hello {me}, fire in the hole!</p>"
+
+@app.route("/hello")
+def get_hello():
+    me ="Anthony"
+    return f"<p>Hello {me}, World!</p>"
+
+@app.route("/goodbye")
+def get_goodbye():
+    me ="Anthony"
+    return f"<p>Bye {me}, come back soon!</p>"
+
+@app.route("/data")
+def get_data():
+    data = [
+        ("name:Freddy", "type:bear"),
+        ("name:Bonnie", "type:bunny"),
+        ("name:Chica", "type:kitchen"),
+        ("name:Foxy", "type:pirate"),
+        ("name:Gabe", "type:colossal")
+    ]
+    return jsonify(data);
+
+@app.route("/<path:path>")
+def serve_static(path):
+    return send_from_directory('.',path)
